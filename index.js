e92460466a2f91629182313042820a68b185f92c
bm9avan <mggpavan@gmail.com>
2023-09-29 17:10:29 +0530

    html on / & /alt

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const shortenerRoute = require("./routes/url");
 const shortRoute = require("./routes/shortId");
 const connectDB = require("./connect");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
 connectDB(process.env.MONGO_URI);
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/frontend")));
 app.use("/url", shortenerRoute);
 app.use("/", shortRoute);
 
